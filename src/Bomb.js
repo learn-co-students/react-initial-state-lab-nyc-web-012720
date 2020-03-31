153bc5900be7b771764ca5676141a7f6465c8710
sekoudosso82 <sekou.dosso82@gmail.com>
2020-03-31 11:45:58 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React, {Component} from 'react'
+
+class Bomb extends Component{
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount 
+        } 
+    }
+    bombRender = () => {
+        console.log('*********************')
+        console.log(this.state.secondsLeft)
+        if (this.state.secondsLeft !== 0){
+            return <div>{this.state.secondsLeft} seconds left before I go boom!</div>
+        }else if (this.state.secondsLeft == 0){
+            return <div>Boom!</div>
+        } 
+    }
+    render(){
+        return (
+            <div>
+                {this.bombRender()}
+            </div>
+        )
+    }
+}
+
+export default Bomb 
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,30 @@
-// your ImageSlider code here!
+import React, {Component} from 'react'
+
+class ImageSlider extends Component{
+    constructor(){
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        } 
+    }
+
+    // changeState = () => {
+    //     console.log(`before setState: ${this.state.currentSlideIndex}`)
+    //     this.setState({
+    //         currentSlideIndex: `I am on slide ${this.state.currentSlideIndex}`  
+    //     })
+    //     console.log(`before setState: ${this.state.currentSlideIndex}`)
+    // }
+
+    render(){
+        return (
+            // <div onClick={this.changeState}>
+            <div >
+                I am on slide {this.state.currentSlideIndex}
+            </div>
+        )
+    }
+}
+
+export default ImageSlider 
+
